b9608971fd38b2d35a1605eae847138cdc85ca0e
Kenza <yahikenza865@gmail.com>
2020-01-07 09:53:24 +0100

    First Commit

---
+++ src/main/java/com/upem/fr/AquariumProjectApplication.java
@@ -0,0 +1,13 @@
+package com.upem.fr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AquariumProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AquariumProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/upem/fr/AquariumProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upem.fr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AquariumProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
