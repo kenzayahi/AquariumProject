e75d865a00918f9b0e5b343dc2196bc0b5071abd
kenyahi <kenza.yahi@mediatransports.com>
2020-01-13 17:31:55 +0100

    ajout du composant Espèce

--- src/main/angular/App/src/app/app.module.ts
+++ src/main/angular/App/src/app/app.module.ts
@@ -12,12 +12,14 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { AnimalComponent } from './animal/animal.component';
+import { EspeceComponent } from './espece/espece.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
     AnimalComponent,
+    EspeceComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/main/angular/App/src/app/espece/espece.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-espece',
+  templateUrl: './espece.component.html',
+  styleUrls: ['./espece.component.css']
+})
+export class EspeceComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
