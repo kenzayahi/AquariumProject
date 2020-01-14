9a07bba7d5d745d227fe74bef974cf1f3d31661f
kenyahi <kenza.yahi@mediatransports.com>
2020-01-14 17:47:50 +0100

    ajout des router link

--- src/main/angular/App/src/app/app-routing.module.ts
+++ src/main/angular/App/src/app/app-routing.module.ts
@@ -1,8 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {AnimalComponent} from './animal/animal.component';
+import {EspeceComponent} from './espece/espece.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'animal',
+    component: AnimalComponent
+  },
+  {
+    path: 'espece',
+    component: EspeceComponent
+  },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
