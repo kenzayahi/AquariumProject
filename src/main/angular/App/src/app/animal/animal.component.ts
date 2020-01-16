6145002fdb5b27e53a57a7d107081d9bdcefd7f8
kenzayahi <yahikenza865@gmail.com>
2020-01-16 23:55:18 +0100

    affiche la list des animaux

--- src/main/angular/App/src/app/animal/animal.component.ts
+++ src/main/angular/App/src/app/animal/animal.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {AnimalService} from "./animal.service";
+import {Animal} from "./animal";
 
 @Component({
   selector: 'app-animal',
@@ -6,10 +8,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./animal.component.css']
 })
 export class AnimalComponent implements OnInit {
+  public animaux: Array<Animal>;
 
-  constructor() { }
+  constructor(private animalService:AnimalService) { }
 
   ngOnInit() {
+  }
+  onGetanimaux(){
+    console.log("bbbbbbb");
+
+    this.animalService.getAnimaux().subscribe(data=>{
+      console.log("hhhhhhhhhh");
+        this.animaux=data;
+        console.log(data);
+        console.log("finish")
+    },error => {
+      console.log(error);
+    })
   }
 
 }
---
+++ src/main/angular/App/src/app/animal/animal.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {Animal} from "./animal";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AnimalService {
+
+  constructor(private httpClient:HttpClient) { }
+
+
+  getAnimaux() : Observable<Array<Animal>> {
+    console.log("hey");
+    return this.httpClient.get<Array<Animal>>('http://localhost:4200/animaux')
+
+  }
+}
