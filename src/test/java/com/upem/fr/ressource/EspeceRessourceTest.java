4e3b62b151a3eaecf43abb6a3a3dcbf0f792494a
YAHI KENZA <yahikenza865@gmail.com>
2020-02-20 14:11:32 +0100

    Correction des tests unit

--- src/test/java/com/upem/fr/ressource/EspeceRessourceTest.java
+++ src/test/java/com/upem/fr/ressource/EspeceRessourceTest.java
@@ -1,7 +1,6 @@
 package com.upem.fr.ressource;
 
 import com.upem.fr.model.Animal;
-import com.upem.fr.model.Bassin;
 import com.upem.fr.model.Espece;
 import com.upem.fr.repository.EspeceRepository;
 import com.upem.fr.service.EspeceService;
@@ -19,7 +18,6 @@ import org.springframework.test.context.junit4.SpringRunner;
 import java.util.List;
 import java.util.Optional;
 
-import static com.upem.fr.model.enumeration.Etat.propre;
 import static com.upem.fr.model.enumeration.RegimeAlimentaire.piscivore;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.doNothing;
@@ -31,7 +29,6 @@ class EspeceRessourceTest {
 
     @LocalServerPort
     private int port;
-
 
     @MockBean
     private EspeceService especeService;
@@ -45,7 +42,6 @@ class EspeceRessourceTest {
     @Test
     void getAll() {
         List especes = this.restTemplate.getForObject("http://localhost:" + port + "/especes", List.class);
-
         assertEquals(0, especes.size());
     }
 
@@ -54,8 +50,9 @@ class EspeceRessourceTest {
         Espece espece = new Espece("Poisson", piscivore, 0);
         espece.setId(1L);
         when(especeService.create(espece)).thenReturn(espece);
-
         Espece result = this.restTemplate.postForObject("http://localhost:" + port + "/especes", espece, Espece.class);
+        System.out.println(espece.toString());
+        System.out.println(result.toString());
         assertEquals(espece, result);
     }
 
@@ -67,7 +64,6 @@ class EspeceRessourceTest {
         HttpEntity<Espece> request = new HttpEntity<>(espece);
         this.restTemplate.exchange("http://localhost:" + port + "/especes",
                 HttpMethod.POST, request, Espece.class);
-
         Espece resultGet = this.restTemplate.getForObject("http://localhost:" + port + "/especes/1", Espece.class);
         assertEquals(espece, resultGet);
     }
@@ -80,34 +76,24 @@ class EspeceRessourceTest {
         when(especeService.getOne(1L)).thenReturn(Optional.of(espece));
         HttpEntity<Espece> request = new HttpEntity<>(espece);
         this.restTemplate.exchange("http://localhost:" + port + "/especes", HttpMethod.POST, request, Animal.class);
-
         List ListBefore= this.restTemplate.getForObject("http://localhost:" + port + "/especes", List.class);
-
         doNothing().when(especeRepository).delete(espece);
         Espece result = this.restTemplate.exchange("http://localhost:" + port + "/especes/1",
                 HttpMethod.DELETE, null, Espece.class).getBody();
         List ListAfter = this.restTemplate.getForObject("http://localhost:" + port + "/especes", List.class);
         assertEquals(ListAfter.size(), ListBefore.size());
-       // assertEquals(result.getMenacee(),espece.getMenacee());
-
-
     }
 
     @Test
     void update() {
         Espece espece = new Espece("Poisson", piscivore, 0);
         espece.setId(1L);
-
         when(especeService.create(espece)).thenReturn(espece);
-
         Espece espece2 = new Espece("Poisson", piscivore, 5);
         espece2.setId(1L);
-
         when(especeService.update(1L, espece2)).thenReturn(espece2);
-
         this.restTemplate.postForObject("http://localhost:" + port + "/especes", espece, Espece.class);
         HttpEntity<Espece> request = new HttpEntity<>(espece2);
-
         Espece result = this.restTemplate.exchange("http://localhost:" + port + "/especes/1",
                 HttpMethod.POST, request, Espece.class).getBody();
 
